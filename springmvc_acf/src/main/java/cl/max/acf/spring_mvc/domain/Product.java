5e8d76c378ffda43522b8cbf08ce4e578567efae
Maximiliano Mussuto <mossmax62@gmail.com>
2014-02-25 13:14:31 -0300

    Dependencias para la persistencia de datos + la interfaz + la implementacion del patron Dao + la anotacion de la clase Product

--- springmvc_acf/src/main/java/cl/max/acf/spring_mvc/domain/Product.java
+++ springmvc_acf/src/main/java/cl/max/acf/spring_mvc/domain/Product.java
@@ -2,6 +2,15 @@ package cl.max.acf.spring_mvc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
 public class Product implements Serializable{
 
 	/**
@@ -9,10 +18,22 @@ public class Product implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
 	private String description;
 	private Double price;
 	
 	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getDescription() {
 		return description;
 	}
